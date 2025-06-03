31c73ceac476abf42d72dbf8a8d0af139f302e0b
nah-nah <152399112+Batcherss@users.noreply.github.com>
2025-06-03 16:48:24 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -9,27 +9,27 @@ import (
 )
 
 func printUsage() {
-	brainrootamobea()
-	fmt.Println("Usage: shellconverter -i <input_file> -o <output_file> -crypt <passphrase> -opt")
+	fmt.Println("Usage: shellconverter -i <input_file> -o <output_file> -crypt <passphrase> -method <aes|chacha20|xor|b64> -opt")
 	fmt.Println()
 	fmt.Println("Arguments:")
 	fmt.Println("-i <input_file>    Input file (e.g., .exe or .dll)")
 	fmt.Println("-o <output_file>   Output file (e.g., .bin)")
-	fmt.Println("-crypt <passphrase> Optional AES encryption passphrase")
-	fmt.Println("-opt                Optional flag to optimize shellcode (removes commas)")
+	fmt.Println("-crypt <passphrase> Encryption passphrase (required for aes, chacha20, xor)")
+	fmt.Println("-method <method>   Encryption method: aes, chacha20, xor, b64 (default aes)")
+	fmt.Println("-opt               Optional flag to optimize shellcode (removes commas)")
 	fmt.Println()
 	fmt.Println("Example:")
-	fmt.Println("shellconverter -i input.exe -o output.bin -crypt=\"mysecurepassword\" -opt")
+	fmt.Println(`shellconverter -i input.exe -o output.bin -crypt="mypassword" -method=aes -opt`)
 }
 
 func brainrootamobea() {
 	banner := []string{
 		"  _________.__           .__  .__                                ",
 		" /   _____/|  |__   ____ |  | |  |       ____  ____   _______  __",
-		" \_____  \ |  |  \_/ __ \|  | |  |     _/ ___\/  _ \ /    \  \/ /",
-		" /        \|   Y  \  ___/|  |_|  |__   \  \__(  <_> )   |  \   / ",
-		"/_______  /|___|  /\___  >____/____/ /\ \___  >____/|___|  /\_/  ",
-		"         \/      \/     \/            \/     \/           \/      ",
+		" \\_____  \\ |  |  \\_/ __ \\|  | |  |     _/ ___\\/  _ \\ /    \\  \\/ /",
+		" /        \\|   Y  \\  ___/|  |_|  |__   \\  \\__(  <_> )   |  \\   / ",
+		"/_______  /|___|  /\\___  >____/____/ /\\ \\___  >____/|___|  /\\_/  ",
+		"         \\/      \\/     \\/            \\/     \\/           \\/      ",
 		"   -brainroot amobea f#cked our brain.",
 	}
 
@@ -40,32 +40,30 @@ func brainrootamobea() {
 
 func main() {
 	brainrootamobea()
-
 	inputFile := flag.String("i", "", "Input file (.exe or .dll)")
 	outputFile := flag.String("o", "", "Output file (.bin)")
-	passphrase := flag.String("crypt", "", "Encryption passphrase (AES)")
+	passphrase := flag.String("crypt", "", "Encryption passphrase")
+	method := flag.String("method", "aes", "Encryption method: aes, chacha20, xor, b64")
 	optimize := flag.Bool("opt", false, "Optimize the shellcode")
 	flag.Parse()
-
 	if *inputFile == "" || *outputFile == "" {
 		printUsage()
 		os.Exit(1)
 	}
-
+	if (*method != "b64") && (*passphrase == "") {
+		fmt.Println("[!] passphrase is required for method:", *method)
+		os.Exit(1)
+	}
 	fmt.Println("[+] received file conversion in progress..")
-
-	shellcode, err := goshellconv.ConvertToShellcode(*inputFile, *passphrase, *optimize)
+	shellcode, err := goshellconv.ConvertToShellcode(*inputFile, *passphrase, *optimize, *method)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("[#] converted to shellcode..")
-
 	err = os.WriteFile(*outputFile, shellcode, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println("[#] writed file - success.")
 	fmt.Println("[+] success convertation to shellcode x64.")
 }
